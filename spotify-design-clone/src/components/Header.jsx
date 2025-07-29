42f2cd5a7bd45d4307d856f52e18a4f9ea806643
Syed Abdur Rahman Uddin <rahmansyed862@gmail.com>
2025-07-30 01:23:20 +0530

    Add spotify-design-clone/src/components/Header.jsx 1753818800166

---
+++ spotify-design-clone/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">Spotify Design</div>
+      <nav className="nav">
+        <a href="#stories">Stories</a>
+        <a href="#tools">Tools</a>
+        <a href="#team">Team</a>
+        <a href="#jobs">Jobs</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
